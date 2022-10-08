c724fe99d3cc73f96026cd9c207a23f1f7753025
L-Bellei <leo14bellei@gmail.com>
2022-10-08 18:51:22 -0400

    updating inventory service

--- Domain/Services/InventoryServices/InventoryService.cs
+++ Domain/Services/InventoryServices/InventoryService.cs
@@ -1,25 +1,24 @@
 ﻿using Library.Domain.Entities;
-using Library.Domain.Repositories.BookRepo.Dtos;
+using Library.Domain.Repositories.BookRepo;
 using Library.Domain.Repositories.InventoryRepo;
 using Library.Domain.Repositories.InventoryRepo.Dtos;
-using Library.Domain.Services.BookServices;
 
 namespace Library.Domain.Services.InventoryServices;
 
 public class InventoryService : IInventoryService
 {
-    private readonly IBookService bookService;
+    private readonly IBookRepository bookRepository;
     private readonly IInventoryRepository inventoryRepository;
 
-    public InventoryService(IBookService bookService, IInventoryRepository inventoryRepository)
+    public InventoryService(IBookRepository bookRepository, IInventoryRepository inventoryRepository)
     {
         this.inventoryRepository = inventoryRepository;
-        this.bookService = bookService;
+        this.bookRepository = bookRepository;
     }
 
     public async Task<InventoryAddResponseDto> AddInventoryAsync(InventoryAddRequestDto inventory)
     {
-        BookGetResponseDto book = await bookService.GetBookByIdAsync(inventory.BookId);
+        Book book = await bookRepository.GetBookById(inventory.BookId);
         Inventory? inventoryExists = await inventoryRepository.GetInventoryByBookIdAsync(book.Id);
 
         if (inventoryExists != null)
@@ -55,7 +54,7 @@ public class InventoryService : IInventoryService
         {
             foreach(var inv in inventories)
             {
-                BookGetResponseDto book = await bookService.GetBookByIdAsync(inv.BookId);
+                Book book = await bookRepository.GetBookById(inv.BookId);
                 InventoryGetResponseDto aux = new()
                 {
                     Id = inv.Id,
@@ -80,7 +79,7 @@ public class InventoryService : IInventoryService
             throw new Exception("Register not found");
         else
         {
-            BookGetResponseDto book = await bookService.GetBookByIdAsync(inventory.BookId);
+            Book book = await bookRepository.GetBookById(inventory.BookId);
 
             return new InventoryGetResponseDto
             {
@@ -95,7 +94,7 @@ public class InventoryService : IInventoryService
     public async Task<InventoryUpdateResponseDto> UpdateInventoryAsync(Guid id, InventoryUpdateRequestDto inventory)
     {
         Inventory? inventoryFinded = await inventoryRepository.GetInventoryByIdAsync(id);
-        BookGetResponseDto book = await bookService.GetBookByIdAsync(inventory.BookId);
+        Book book = await bookRepository.GetBookById(inventory.BookId);
 
         if (inventoryFinded == null)
             throw new Exception("Register not found");
